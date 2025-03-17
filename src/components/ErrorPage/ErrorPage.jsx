3a233d1294d2c5ca86be4d4fdb1db5e4ecb4fa88
devalamin <alamin.hasibmd2017@gmail.com>
2025-03-18 00:44:39 +0300

    'initialized'

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h5>Something is wrong</h5>     
+            
+            <p>page not found</p>
+               </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from '../Navbar/NavBar';
+import Footer from '../Footer/Footer';
+import { Outlet } from 'react-router-dom';
+
+const Root = () => {
+    return (
+        <div>
+           
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
